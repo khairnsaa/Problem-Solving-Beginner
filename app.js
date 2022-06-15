e0f499a8d6404105b515e28cc04ddb6990dfaffe
khairnsaa <k.nsaa95@gmail.com>
2022-06-15 19:55:55 +0700

    add diagonal difference code to app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const diagonalDifference
+const diagonalDifference = (arr) => {
+    let ltrTotal = 0
+    let rtlTotal = 0
+    
+    for(let i=0; i<arr.length; i++){
+        for(let j=i; j <=i; j++){
+            ltrTotal += arr[i][j]          
+        }
+    }
+    
+    for(let k=0; k<arr.length; k++ ){
+        for(let l=arr.length-1-k; l<arr.length-k; l++){
+            rtlTotal += arr[k][l]
+            
+        }
+    }
+    return Math.abs(ltrTotal - rtlTotal)
+}
